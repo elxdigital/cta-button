973f27d1cb3c05632a68b63a68991d9404b916c6
dev-ellox <desenvolvimento@ellox.com.br>
2025-07-25 11:25:54 -0300

    Adicionando dependência para teste de exemplos

--- examples/dep/dependencias.php
+++ examples/dep/dependencias.php
@@ -2,3 +2,5 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- jQuery Mask -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+<!-- Bootstrap 4 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
