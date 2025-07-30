30fe917293c406bde11737b5dc291a582a24cc0e
dev-ellox <desenvolvimento@ellox.com.br>
2025-07-30 10:18:01 -0300

    Ajustando rotas de scripts

--- src/template/public.php
+++ src/template/public.php
@@ -12,9 +12,11 @@ $button = new \Elxdigital\CtaButton\Domain\Button($btn_conversao);
 echo $button->getFunction($arrayClasses, $span);
 
 if ($hasModal && !empty($form)) {
+    $dir = __DIR__;
+
     echo <<<HTML
-        <script rel="text/javascript" src="../src/template/assets/script_public.js" defer></script>
-        <link rel="stylesheet" href="../src/template/assets/style_public.css">
+        <script rel="text/javascript" src="{$dir}/../src/template/assets/script_public.js" defer></script>
+        <link rel="stylesheet" href="{$dir}/../src/template/assets/style_public.css">
         
         <div id="modalOverlay" style="display:none; position:fixed; top:0; left:0; width:100vw; height:100vh; background:rgba(0,0,0,0.5); z-index:9998;"></div>
         <div id="form-{$button->getBtnIdentificador()}" class="modal-button-cta" style="display:none; position:fixed; top:50%; left:50%; transform:translate(-50%,-50%); background:#fff; padding:2rem; border-radius:10px; box-shadow:0 2px 20px #333; z-index:9999; min-width:320px;">
